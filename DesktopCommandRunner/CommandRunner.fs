276570971ad0ce1070b2e7b35a89d7a9d98ad34c
Amine Mejaouel <45773049+amine-mejaouel@users.noreply.github.com>
2021-12-05 16:58:41 +0100

    Refactor FuzzyAutoCompleteBox into its own module

--- DesktopCommandRunner/CommandRunner.fs
+++ DesktopCommandRunner/CommandRunner.fs
@@ -1,89 +1,39 @@
 ﻿namespace DesktopCommandRunner
 
-open System
 open Avalonia.Controls
 open Avalonia.FuncUI.DSL
 open Avalonia.Layout
-open FuzzySharp
-open FuzzySharp.SimilarityRatio
-open FuzzySharp.SimilarityRatio.Scorer.Composite
+open FuzzyAutoCompleteBox
 
 module CommandRunner =
-    
-    type State =
-        { Commands : Command list
-          SelectedCommand : Command option
-          Msg : string }
-    
-    let init =
-        { Commands= []
-          SelectedCommand= None
-          Msg= "" }
 
-    type Msg =
-        | UpdateMatchedCommands of string
-        | UpdateSelectedCommand of string option
-        | RunCommand
+  type State =
+    { CommandsState: FuzzyAutoCompleteBoxState }
 
-    let update (msg: Msg) (state: State) : State =
-        match msg with
-        | UpdateMatchedCommands input ->
-            if input.Length < 3 then
-                { state with Commands= List.empty }
-            else
-                let identifyMatchedCommands =
-                    let sortedCommands = commands |> List.sort
-                    fun input -> 
-                        sortedCommands
-                        |> Seq.zip
-                               ( Process.ExtractAll(
-                                    input,
-                                    sortedCommands |> List.map (fun c -> c.Name),
-                                    scorer= ScorerCache.Get<WeightedRatioScorer>())
-                                 |> Seq.map (fun r -> r.Score))
-                        |> Seq.sortByDescending fst
-                        |> Seq.map snd
-                        |> Seq.toList
-            
-                let matchedCommands = identifyMatchedCommands input
-                { state with Commands = matchedCommands }
-            
-        | UpdateSelectedCommand input ->
-            match input with
-            | Some cmdName -> 
-                { state with SelectedCommand= state.Commands |> List.tryFind (fun c -> c.Name = cmdName) }
-            | None -> { state with SelectedCommand= None }
-            
-        | RunCommand ->
-            match state.SelectedCommand with
-            | Some command -> { state with Msg= sprintf "%A Run already selected command %A" DateTime.Now.Millisecond command }
-            | None -> { state with Msg= sprintf "%A Run best matched command %A" (DateTime.Now.Millisecond) (state.Commands |> List.tryHead) }
-            
-    let view (state: State) (dispatch) =
-        DockPanel.create [
-            DockPanel.children [
-                AutoCompleteBox.create [
-                    AutoCompleteBox.dock Dock.Top
-                    AutoCompleteBox.fontSize 24.0
-                    AutoCompleteBox.verticalAlignment VerticalAlignment.Center
-                    AutoCompleteBox.horizontalAlignment HorizontalAlignment.Stretch
-                    AutoCompleteBox.dataItems (state.Commands |> List.map (fun c -> c.Name))
-                    AutoCompleteBox.filterMode AutoCompleteFilterMode.Custom
-                    AutoCompleteBox.itemFilter (fun (_:string) (_:obj) -> true)
-                    AutoCompleteBox.onSearchTextChanged (fun searchText ->
-                        dispatch (UpdateMatchedCommands searchText))
-                    AutoCompleteBox.onSelectedItemChanged (fun item ->
-                        match item with
-                        | :? string as commandName -> dispatch (UpdateSelectedCommand (Some commandName))
-                        | _ -> dispatch (UpdateSelectedCommand None))
-                    AutoCompleteBox.onKeyDown (fun e ->
-                        match e.Key with
-                        | Avalonia.Input.Key.Enter -> dispatch RunCommand
-                        | _ -> ())
-                ]       
-                Button.create [
-                    Button.dock Dock.Bottom
-                    Button.content state.Msg
-                ]                
-            ]
-        ]       
+  let init =
+    { CommandsState =
+        { InitialState with
+            Items = seq {
+              for i in [1..10] -> { DisplayText = (sprintf "Sample command %i" i) }
+            } |> List.ofSeq } }
+
+  type Msg = FuzzyAutoCompleteBoxMsg of FuzzyAutoCompleteBoxMsg
+
+  let update (msg: Msg) (state: State) : State =
+    match msg with
+    | FuzzyAutoCompleteBoxMsg msg ->
+      { state with
+          CommandsState = update msg state.CommandsState }
+
+
+  let view (state: State) (dispatch) =
+    DockPanel.create [
+      DockPanel.children [
+        FuzzyAutoCompleteBox.create state.CommandsState (FuzzyAutoCompleteBoxMsg >> dispatch)
+          [ AutoCompleteBox.dock Dock.Top
+            AutoCompleteBox.background "Red"
+            AutoCompleteBox.fontSize 24.0
+            AutoCompleteBox.verticalAlignment VerticalAlignment.Center
+            AutoCompleteBox.horizontalAlignment HorizontalAlignment.Stretch ]
+        TextBlock.create
+          [ TextBlock.text state.CommandsState.DEBUG_MSG ]] ]
---
+++ DesktopCommandRunner/FuzzyAutoCompleteBox.fs
@@ -0,0 +1,96 @@
+module FuzzyAutoCompleteBox
+
+open System
+open System.Collections.Generic
+open Avalonia.Controls
+open Avalonia.FuncUI.Builder
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Types
+open FuzzySharp
+open FuzzySharp.SimilarityRatio
+open FuzzySharp.SimilarityRatio.Scorer.Composite
+
+type Item =
+    { DisplayText: string }
+    
+type FuzzyAutoCompleteBoxState = {
+    Items: Item list
+    FilteredItems: Item list
+    SelectedItem: Item option
+    DEBUG_MSG : string
+}
+
+let InitialState = {
+    Items = list.Empty
+    FilteredItems = list.Empty
+    SelectedItem = None
+    DEBUG_MSG = "init"
+}
+    
+type FuzzyAutoCompleteBoxMsg =
+    | UpdateSelectedItem of string option
+    | UpdateMatchedItems of string
+    | RunSelectedItem
+    
+type FuzzyAutoCompleteBox() =
+    inherit AutoCompleteBox()
+    
+type FuzzyAutoCompleteBox with        
+    static member dataItems<'t when 't :> AutoCompleteBox>(items: IEnumerable<Item>) : IAttr<'t> =
+        AttrBuilder<'t>.CreateProperty<System.Collections.IEnumerable>(
+            AutoCompleteBox.ItemsProperty,
+            (items |> Seq.map (fun c -> c.DisplayText)) ,
+            ValueSome (fun (input: obj*obj) ->
+                let s1 = input |> fst :?> IEnumerable<_>
+                let s2 = input |> snd :?> IEnumerable<_>
+                Seq.length s1 = Seq.length s2 && Seq.forall2 (fun i1 i2 -> i1.Equals i2) s1 s2))
+        
+let update (msg: FuzzyAutoCompleteBoxMsg) (state: FuzzyAutoCompleteBoxState) : FuzzyAutoCompleteBoxState =
+    match msg with
+    | UpdateMatchedItems input ->
+        if input.Length < 3 then
+            { state with FilteredItems= List.empty }
+        else
+            let identifyMatchedCommands =
+                let sortedCommands = state.Items |> List.sort
+                fun input -> 
+                    sortedCommands
+                    |> Seq.zip
+                           ( Process.ExtractAll(
+                                input,
+                                sortedCommands |> List.map (fun c -> c.DisplayText),
+                                scorer= ScorerCache.Get<WeightedRatioScorer>())
+                             |> Seq.map (fun r -> r.Score))
+                    |> Seq.sortByDescending fst
+                    |> Seq.map snd
+        
+            let filteredItems = identifyMatchedCommands input
+            { state with FilteredItems = filteredItems |> Seq.take 5 |> Seq.toList }
+        
+    | UpdateSelectedItem displayText ->
+        match displayText with
+        | Some displayTextValue -> 
+            { state with SelectedItem= state.FilteredItems |> List.tryFind (fun c -> c.DisplayText = displayTextValue) }
+        | None -> { state with SelectedItem= None }
+        
+    | RunSelectedItem ->
+        match state.SelectedItem with
+        | Some item -> { state with DEBUG_MSG= sprintf "%A Run already selected command %A" DateTime.Now.Millisecond item }
+        | None -> { state with DEBUG_MSG= sprintf "%A Run best matched command %A" (DateTime.Now.Millisecond) (state.FilteredItems |> List.tryHead) }
+
+let create (state: FuzzyAutoCompleteBoxState) dispatch (attrs: IAttr<AutoCompleteBox> list): IView<AutoCompleteBox> =
+    AutoCompleteBox.create (attrs@[
+          FuzzyAutoCompleteBox.dataItems state.FilteredItems
+          FuzzyAutoCompleteBox.filterMode AutoCompleteFilterMode.Custom
+          FuzzyAutoCompleteBox.itemFilter (fun (_:string) (_:obj) -> true)
+          FuzzyAutoCompleteBox.onSearchTextChanged (fun searchText ->
+                        dispatch (UpdateMatchedItems searchText))
+          FuzzyAutoCompleteBox.onSelectedItemChanged (fun item ->
+                        match item with
+                        | :? string as commandName -> dispatch (UpdateSelectedItem (Some commandName))
+                        | _ -> dispatch (UpdateSelectedItem None))
+          FuzzyAutoCompleteBox.onKeyDown (fun e ->
+                        match e.Key with
+                        | Avalonia.Input.Key.Enter -> dispatch RunSelectedItem
+                        | _ -> ()) ])
+
